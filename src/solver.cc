47851eec9760b389f783334816c06e7a33b0bff5
Joseph Davis <josephdavisjoey@gmail.com>
2023-04-09 02:49:13 -0400

    Updated variable names, added functions and attributes. Inlined all functions. Added preprocessor/macros to prevent redefining the class.

--- src/solver.cc
+++ src/solver.cc
@@ -1,45 +1,71 @@
+#ifndef SUDOKU_BOARD_BACKEND
+#define SUDOKU_BOARD_BACKEND
 #include <algorithm>
 #include <cstddef>
 #include <vector>
 #include <iostream>
 class sudoku_board {
 	public:
-		sudoku_board(std::vector<std::vector<int>> _board) : board(_board) {show_board();}
-		void show_board();
-		static bool replacement_possible(int _row, int _col, int n, std::vector<std::vector<int>> board);
-		void solve(unsigned int method = 0) {
+		sudoku_board(std::vector<std::vector<int>> _board_values)
+			: board_values(_board_values)
+		{
+			for (int r = 0; r < 9; r++) {
+				std::vector<bool> temp_row;
+				for (int c = 0; c < 9; c++) temp_row.push_back(board_values[r][c] == 0);
+				replacement_mask.push_back(temp_row);
+			};
+	//		for (auto r : replacement_mask) { for (auto c : r) {std::cout << c << ",";} std::cout << "\n";};
+		};
+		inline void show_board();
+		inline static bool replacement_possible(int _row, int _col, int n, std::vector<std::vector<int>> _board);
+		inline void solve(unsigned int method = 0) {
 			if (method == 0) solver_2();
-			else if (method == 1 or method !=0) board = solver(board);
+			else if (method == 1 or method !=0) board_values = solver(board_values);
 		}
-		bool is_valid() {return validator(board);};
-		static bool validator(std::vector<std::vector<int>> _board);
-		std::vector<int> get_column(unsigned int _column);
-		std::vector<int> get_row(unsigned int _row);
-		std::vector<int> get_unit(unsigned int _unit);
+		inline bool is_valid() {return validator(board_values);};
+		inline static bool validator(std::vector<std::vector<int>> _board_values);
+		inline std::vector<int> get_column(unsigned int _column);
+		inline std::vector<int> get_row(unsigned int _row);
+		inline std::vector<int> get_unit(unsigned int _unit);
+		inline bool update_cell(unsigned int _row, unsigned int _col, int n);
+		std::vector<std::vector<int>> board_values;
 	private:
-		std::vector<std::vector<int>> board;
-		static std::vector<std::vector<int>> solver(std::vector<std::vector<int>> _board);
-		static bool checkNine(std::vector<int> _vector);
-		bool solver_2();
+		std::vector<std::vector<bool>> replacement_mask;
+		inline static std::vector<std::vector<int>> solver(std::vector<std::vector<int>> _board_values);
+		inline static bool checkNine(std::vector<int> _vector);
+		inline bool valid_cell(unsigned int _row, unsigned int _col);
+		inline bool solver_2();
+};
+//checks if the space is valid for replacement. (i.e. if the cell is in the replacement mask)
+bool sudoku_board::valid_cell(unsigned int _row, unsigned int _col) {
+	return replacement_mask[_row][_col];
+}
+//updates the cell if the specific value is possible
+bool sudoku_board::update_cell(unsigned int _row, unsigned int _col, int n){
+	if (valid_cell(_row,_col)) { // or replacement_possible(_row, _col, n,board_values))) {
+		board_values[_row][_col] = n;
+		return true;
+	}
+	else return false;
 };
 //Returns the specified column as a vector, from row 0 to 8
 std::vector<int> sudoku_board::get_column(unsigned int _column) {
 	return(std::vector<int>({
-		board[0][_column],board[1][_column],
-		board[2][_column],board[3][_column],
-		board[4][_column],board[5][_column],
-		board[6][_column],board[7][_column],
-		board[7][_column],board[8][_column]
+		board_values[0][_column],board_values[1][_column],
+		board_values[2][_column],board_values[3][_column],
+		board_values[4][_column],board_values[5][_column],
+		board_values[6][_column],board_values[7][_column],
+		board_values[7][_column],board_values[8][_column]
 	}));
 }
 //Returns the specified row as a vector, from column 0 to 8
 std::vector<int> sudoku_board::get_row(unsigned int _row) {
 	return(std::vector<int>({
-		board[_row][0],board[_row][1],
-		board[_row][2],board[_row][3],
-		board[_row][4],board[_row][5],
-		board[_row][6],board[_row][7],
-		board[_row][7],board[_row][8]
+		board_values[_row][0],board_values[_row][1],
+		board_values[_row][2],board_values[_row][3],
+		board_values[_row][4],board_values[_row][5],
+		board_values[_row][6],board_values[_row][7],
+		board_values[_row][7],board_values[_row][8]
 	}));
 }
 //returns the specified unit as a vector, left-right then top-down
@@ -59,32 +85,32 @@ std::vector<int> sudoku_board::get_unit(unsigned int _unit) {
 		}
 		//
 		return(std::vector<int>({
-			board[0 + off_y][0 + off_x],board[0 + off_y][1 + off_x],board[0 + off_y][2 + off_x],
-			board[1 + off_y][0 + off_x],board[1 + off_y][1 + off_x],board[1 + off_y][2 + off_x],
-			board[2 + off_y][0 + off_x],board[2 + off_y][1 + off_x],board[2 + off_y][2 + off_x]
+			board_values[0 + off_y][0 + off_x],board_values[0 + off_y][1 + off_x],board_values[0 + off_y][2 + off_x],
+			board_values[1 + off_y][0 + off_x],board_values[1 + off_y][1 + off_x],board_values[1 + off_y][2 + off_x],
+			board_values[2 + off_y][0 + off_x],board_values[2 + off_y][1 + off_x],board_values[2 + off_y][2 + off_x]
 		}));
 }
 void sudoku_board::show_board() {
 	std::cout << "\n";
-	for (int row = 0; row < board.size(); row++) {
-		for (int col = 0; col < board[row].size(); col++)
-			std::cout << ((board[row][col] == 0)?"+":std::to_string(board[row][col])) << " ";
+	for (int row = 0; row < board_values.size(); row++) {
+		for (int col = 0; col < board_values[row].size(); col++)
+			std::cout << ((board_values[row][col] == 0)?"+":std::to_string(board_values[row][col])) << " ";
 		std::cout << "\n";
 	}
 }
-bool sudoku_board::validator(std::vector<std::vector<int>> _board) {
+bool sudoku_board::validator(std::vector<std::vector<int>> _board_values) {
 	//get_row/get_column/get_unit are not being used because they aren't static.
 	//Validate Rows
-	for (std::vector<int>& row : _board)
+	for (std::vector<int>& row : _board_values)
 		if (checkNine(row) == false) return false;
 	//Validate Columns
 	for (int c = 0; c < 9; c++) {
 		std::vector<int> col_values = {
-			_board[0][c],_board[1][c],
-			_board[2][c],_board[3][c],
-			_board[4][c],_board[5][c],
-			_board[6][c],_board[7][c],
-			_board[7][c],_board[8][c]
+			_board_values[0][c],_board_values[1][c],
+			_board_values[2][c],_board_values[3][c],
+			_board_values[4][c],_board_values[5][c],
+			_board_values[6][c],_board_values[7][c],
+			_board_values[7][c],_board_values[8][c]
 		};
 		if (checkNine(std::move(col_values)) == false) return false;
 	}
@@ -105,11 +131,11 @@ bool sudoku_board::validator(std::vector<std::vector<int>> _board) {
 		}
 		//
 		const std::vector<int> unit_values = {
-			_board[0 + off_y][0 + off_x],_board[0 + off_y][1 + off_x],
-			_board[0 + off_y][2 + off_x],_board[1 + off_y][0 + off_x],
-			_board[1 + off_y][1 + off_x],_board[1 + off_y][2 + off_x],
-			_board[2 + off_y][0 + off_x],_board[2 + off_y][1 + off_x],
-			_board[2 + off_y][2 + off_x]
+			_board_values[0 + off_y][0 + off_x],_board_values[0 + off_y][1 + off_x],
+			_board_values[0 + off_y][2 + off_x],_board_values[1 + off_y][0 + off_x],
+			_board_values[1 + off_y][1 + off_x],_board_values[1 + off_y][2 + off_x],
+			_board_values[2 + off_y][0 + off_x],_board_values[2 + off_y][1 + off_x],
+			_board_values[2 + off_y][2 + off_x]
 		};
 		if (checkNine(std::move(unit_values)) == false) return false;
 	}
@@ -125,37 +151,37 @@ bool sudoku_board::checkNine(std::vector<int> _vector) {
 		and values[6] and values[7]
 		and values[8]); 
 }
-bool sudoku_board::replacement_possible(int _row, int _col, int n, std::vector<std::vector<int>> board) {
+bool sudoku_board::replacement_possible(int _row, int _col, int n, std::vector<std::vector<int>> _board) {
 	for (int r = 0; r < 9; r++) {
-		if (board[r][_col] == n) return false;
+		if (_board[r][_col] == n) return false;
 	}
 	for (int c = 0; c < 9; c++) {
-		if (board[_row][c] == n) return false;
+		if (_board[_row][c] == n) return false;
 	}
 	int y_offset = (_row/3)*3;
 	int x_offset = (_col/3)*3;
 	for (int y = 0; y < 3; y++) for (int x = 0; x < 3; x++) {
-		if (board[y_offset+y][x_offset+x] == n) return false;
+		if (_board[y_offset+y][x_offset+x] == n) return false;
 	}
 	//
 	return true;
 }
 //solves the board using recursion, but passes the board along to each call. Likely inefficient
-std::vector<std::vector<int>> sudoku_board::solver(std::vector<std::vector<int>> _board) {
-	if (_board.empty()) return _board;
+std::vector<std::vector<int>> sudoku_board::solver(std::vector<std::vector<int>> _board_values) {
+	if (_board_values.empty()) return _board_values;
 	for (int r = 0; r < 9; r++) for (int c = 0; c < 9; c++) {
-		if (_board[r][c] == 0) {
+		if (_board_values[r][c] == 0) {
 			for (int n = 1; n < 10; n++) {
-				if (replacement_possible(r,c,n,_board) == true) {
-					_board[r][c] = n;
-					auto s = solver(_board);
-					if (s.empty()) _board[r][c] = 0;
+				if (replacement_possible(r,c,n,_board_values) == true) {
+					_board_values[r][c] = n;
+					auto s = solver(_board_values);
+					if (s.empty()) _board_values[r][c] = 0;
 					else return s;
 				}
 			} return {}; 
 		}
 	} 
-	return _board;
+	return _board_values;
 }
 //solves the board using recursion directly on the classes board. Likely efficient
 bool sudoku_board::solver_2() {
@@ -163,12 +189,12 @@ bool sudoku_board::solver_2() {
 	if (is_valid() == true)	return 1;	
 	//iterate over every cell in the board
 	for (int r = 0; r<9; r++) for (int c = 0; c < 9 ; c++) {
-		if (board[r][c] == 0) { //if intended for replacement
+		if (board_values[r][c] == 0) { //if intended for replacement
 			for (int n = 1; n <= 9; n++) { //attempt all values in each replacement position
-				board[r][c] = n; //attempt w/ n
+				board_values[r][c] = n; //attempt w/ n
 				auto s = solver_2(); //recursion. Contnues branching
 				if(s) return s;
-				board[r][c] = 0; //reset back to 0 if n did not work
+				board_values[r][c] = 0; //reset back to 0 if n did not work
 			}
 			//this point is reached only after attemting all replacements
 		}
@@ -204,3 +230,4 @@ bool sudoku_board::solver_2() {
 //	s.show_board();
 //	return 1;
 //}
+#endif
